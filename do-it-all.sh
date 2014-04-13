7cc6ed12a4d1a5b3d3aa3a37b9c78ade9dd62f49
Golden Dave <dave@theboss.com>
2014-04-13 13:39:09 +0100

    fixed typo of incomplete cleo rep url

--- do-it-all.sh
+++ do-it-all.sh
@@ -1,6 +1,6 @@
 apt-get install php5 git -y --force-yes
 cd /tmp
-git clone https://
+git clone https://phpengine@bitbucket.org/phpengine/cleopatra.git
 php cleopatra/install-silent
 git clone https://github.com/phpengine/transform-uk.git
 cd transform-uk
